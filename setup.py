9b9afe08093bf24274b64c33bfc1cc3bee0e7018
Gregory Ashton <gash789@gmail.com>
2015-05-11 11:33:01 +0100

    Fix bug in installation of api key
    This fixes an error where the API key is installed in the local
    directory and not the home directory. Also PEP8 and remove unused
    modules from setup.py and make the command line take all the arguments

--- setup.py
+++ setup.py
@@ -1,11 +1,8 @@
 from setuptools import setup
-import shutil
-import os
-
 
 setup(name='pyweather',
       packages=[''],
       version='1.0',
-      install_requires = ["python-forecastio", "geopy"],
+      install_requires=["python-forecastio", "geopy"],
       entry_points={'console_scripts': ['forecast = forecast:main']}
       )
